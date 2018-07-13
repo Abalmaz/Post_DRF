c0d6ff32f254464a1b2a95ca55d7df71ffc1b432
abalmazik <yana.abalmaz@gmail.com>
2018-07-13 23:45:46 +0300

    Change view for Post and Category to ModelViewSet

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,4 @@
-from django.http import Http404
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import mixins
 from rest_framework import generics
-from rest_framework.views import APIView
 from posts.models import Post, Category
 from django.contrib.auth.models import User
 from posts.serializers import PostSerializer, CategorySerializer, UserSerializer
@@ -11,111 +6,24 @@ from posts.serializers import PostSerializer, CategorySerializer, UserSerializer
 # Create your views here.
 
 
-class PostList(APIView):
-    def get(self, request, format=None):
-        """
-        Return a list of all posts.
-        """
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        """
-        Create a new post.
-        """
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serialize_class = PostSerializer
 
 
-class PostDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        """
-        Retrieve a post instance
-        """
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def patch(self, request, pk, format=None):
-        """
-        Partial update a post instance
-        """
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        """
-        Delete a post instance
-        """
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
-class CategoryList(mixins.ListModelMixin,
-                   mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    def get(self, request, *args, **kwargs):
-        """
-        Return a list of all Categories
-        """
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        """
-        Create a new Category, If an object is created this returns a '201 Created' response
-        else if the request was invalid return a '400 Bad Request'
-        """
-        return self.create(request, *args, **kwargs)
-
-
-class CategoryDetail(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-    def get(self, request, *args, **kwargs):
-        """
-        Retrieve a category instance
-        If an object can be retrieved this returns a '200 OK' response,
-        else '404 Not Found'
-        """
-        return self.retrieve(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        """
-        Partial update a category instance,
-        If an object is updated this returns a 200 OK response,
-        else returns '400 Bad Request' with error details
-        """
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        """
-        Delete a category instance,
-        If an object is deleted this returns a '204 No Content' response,
-        else '404 Not Found'
-        """
-        return self.destroy(request, *args, **kwargs)
 
 
 class UserList(generics.ListCreateAPIView):
